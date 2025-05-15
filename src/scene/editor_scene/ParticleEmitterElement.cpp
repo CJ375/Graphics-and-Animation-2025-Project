1c8552b48ede98abf5befe69ae395255f044a523
charlie <charliej375@gmail.com>
2025-05-15 19:00:20 +0800

    Initial class setup - particles

---
+++ src/scene/editor_scene/ParticleEmitterElement.cpp
@@ -0,0 +1,4 @@
+#include "ParticleEmitterElement.h"
+#include "rendering/imgui/ImGuiManager.h" // For ImGui widgets
+#include "utility/Random.h" // For random number generation
+#include <glm/gtc/type_ptr.hpp> // For ImGui::ColorEdit4
---
+++ src/scene/editor_scene/ParticleEmitterElement.h
@@ -0,0 +1,9 @@
+#ifndef PARTICLE_EMITTER_ELEMENT_H
+#define PARTICLE_EMITTER_ELEMENT_H
+
+#include "SceneElement.h"
+#include "scene/SceneContext.h"
+#include "rendering/resources/TextureHandle.h"
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
