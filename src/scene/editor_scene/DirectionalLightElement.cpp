9aae6af51f929c238a099fc02d3af807a9361559
charlie <charliej375@gmail.com>
2025-05-18 16:50:41 +0800

    Initialised DirectionalLightElement

---
+++ src/scene/editor_scene/DirectionalLightElement.cpp
@@ -0,0 +1,7 @@
+#include "DirectionalLightElement.h"
+
+#include "glm/gtx/transform.hpp"
+#include "glm/gtx/component_wise.hpp"
+
+#include "rendering/imgui/ImGuiManager.h"
+#include "scene/SceneContext.h"
