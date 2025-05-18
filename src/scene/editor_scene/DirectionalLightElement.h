915d64029c291b6707bbcc48448ddab08ef98de5
charlie <charliej375@gmail.com>
2025-05-18 16:51:57 +0800

    Further updates

--- src/scene/editor_scene/DirectionalLightElement.h
+++ src/scene/editor_scene/DirectionalLightElement.h
@@ -1,6 +1,19 @@
 #ifndef DIRECTIONAL_LIGHT_ELEMENT_H
 #define DIRECTIONAL_LIGHT_ELEMENT_H
 
+#include "SceneElement.h"
+#include "scene/SceneContext.h"
 
+namespace EditorScene {
+    class DirectionalLightElement : public SceneElement {
+        public:
+            static constexpr const char* ELEMENT_TYPE_NAME = "Directional Light";
+
+            // Local transformation
+            glm::vec3 position;
+            
+        
+    }
+}
 
 #endif // DIRECTIONAL_LIGHT_ELEMENT_H
